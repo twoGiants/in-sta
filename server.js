5651eb44c331df8d80b4ec5c8323a9a2fb75077e
Stanislav Jakuschevskij <Stas@giantBook.local>
2016-01-29 16:17:11 +0100

    testing: changed time in loop to 16:20

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ loop();
 
 function loop() {
     var settings = {
-        desiredTime: [4, 30],
+        desiredTime: [16, 20],
         usernames: ['stazzmatazz', 'instagram', 'taylorswift', 'selenagomez', 'kimkardashian'],
         source: "http://iconosquare.com/",
         selector: [
