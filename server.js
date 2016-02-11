de6a4408101b03944b94727a3d76422df3cd0834
Yuri Gagarin <2g@space.pew>
2016-02-11 08:47:56 +0100

    refac: restructured server.js a little bit

--- server.js
+++ server.js
@@ -26,43 +26,110 @@ var cheerio = require("cheerio");
 var request = require("request");
 
 // configuration ===============================================================
+process.env.TZ = 'Europe/Berlin';
 // OPENSHIFT-SERVER-DB-CONFIG
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var connectionStringMongoDB = '127.0.0.1:27017/nodejs'; // mongoDB connection string on localhost 
-process.env.TZ = 'Europe/Berlin';
-
+var connectionStringMongoDB;
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionStringMongoDB = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
         process.env.OPENSHIFT_APP_NAME;
+} else {
+    connectionStringMongoDB = '127.0.0.1:27017/nodejs';
 }
 var db = mongojs(connectionStringMongoDB, ['instagram']);
 
 app.use(express.static(__dirname + '/app'));
 app.use(bodyParser.json());
 
-// start server
+// routes ======================================================================
+    // api ---------------------------------------------------------------------
+    // get all data
+    app.get("/statistics", function (err, res) {
+        log("I received a GET request from tableCtrl.");
+
+        db.instagram.find(function (err, docs) {
+            res.json(docs);
+        });
+    });
+
+    app.get('/test', function (err, res) {
+        log('I received a GET erquest from /test.');
+    });
+
+// start app ===================================================================
 app.listen(port, ipaddress, function () {
     log('Server running on http://' + ipaddress + ':' + port);
 });
+loop();
 
-// get all data from DB, send back to FE
-app.get("/statistics", function (err, res) {
-    log("I received a GET request from tableCtrl.");
+// other =======================================================================
+// get data from iconosquare
+function loop() {
+    var iterations = 1;
+    var settings = {
+        desiredTime: [04, 05],
+        usernames: ['stazzmatazz', 'instagram', 'taylorswift', 'selenagomez', 'kimkardashian'],
+        source: "http://iconosquare.com/",
+        selector: [
+            'a[class="followers user-action-btn"] span[class=chiffre]',
+            'a[class="followings user-action-btn"] span[class=chiffre]'
+        ]
+    };
 
-    db.instagram.find(function (err, docs) {
-        res.json(docs);
+    // LOOP: grab data from source
+    async.forever(function (next) {
+        log('Loop running the ' + iterations++ + ' time.');
+        log('Delay: ' + delayInMs(settings.desiredTime) + 'ms.');
+        setTimeout(function() {
+            log('...grabbing data.');
+            getRemoteData(settings.source, settings.usernames[0], settings.selector, next);
+        }, delayInMs(settings.desiredTime)); // 5000 delayInMs(settings.desiredTime)
+    }, function (err) {
+        // error handling
+        error(err.message);
     });
-});
+}
 
-app.get('/test', function (err, res) {
-    log('I received a GET erquest from /test.');
-});
+// get data from source, save data to DB
+function getRemoteData(source, username, selector, callback) {
+    var userUrl = source + username;
+    // send request
+    request(userUrl, {
+            timeout: 10000
+        },
+        function (err, res, body) {
+            if (err) {
+                error(err.message);
+            } else {
+                if (res.statusCode === 200) {
+                    var $ = cheerio.load(body);
+                    var timestamp = new Date();
+                    var newData = {
+                        timestamp: timestamp.getTime().toString(),
+                        followers: $(selector[0]).html().toString(),
+                        followings: $(selector[1]).html().toString()
+                    };
 
-// DB -------------------------------------------------------------
+                    // print data to console
+                    log('Source: ' + userUrl);
+                    log(JSON.stringify(newData, null, '\t'));
+
+                    // save data
+                    saveData(newData, username);
+                } else {
+                    error(res.statusCode);
+                }
+            }
+            // continue the loop
+            callback();
+        }
+    );
+}
+
 // update !existing! user, works only with existing users
 function saveData(newData, username) {
     db.instagram.findAndModify({
@@ -83,9 +150,41 @@ function saveData(newData, username) {
         if (err) {
             error(err.message);
         } else {
-            log('Saved data to DB.\n\n');
+            log('...data saved.\n\n');
         }
     });
+}
+
+/*
+ expects an array desiredTime = [hh, mm]
+ returns the delay neccessary in ms till the desiredTime
+ Formula: 
+ x: current time in ms
+ y: desired time in ms
+ x >= y: (24 - x) + y
+ x <  y: y - x
+ */
+function delayInMs(desiredTime) {
+    var currentTime = new Date();
+    var x = (currentTime.getHours() * 60 + currentTime.getMinutes()) * 60 * 1000;
+    var y = (desiredTime[0] * 60 + desiredTime[1]) * 60 * 1000;
+
+    if (x >= y) {
+        return (86400000 - x) + y;
+    } else {
+        return y - x;
+    }
+}
+
+function delayInMsDEBUG(desiredTime, currentTime) {
+    var x = (currentTime[0] * 60 + currentTime[1]) * 60 * 1000;
+    var y = (desiredTime[0] * 60 + desiredTime[1]) * 60 * 1000;
+
+    if (x >= y) {
+        return (86400000 - x) + y;
+    } else {
+        return y - x;
+    }
 }
 
 // deletes one entry every second
@@ -130,101 +229,6 @@ function insertData() {
         if (err) error(err.message);
         log(JSON.stringify(doc, null, '\t'));
     });
-}
-
-// OTHER ----------------------------------------------------------
-// get data from iconosquare
-loop();
-function loop() {
-    var settings = {
-        desiredTime: [04, 05],
-        usernames: ['stazzmatazz', 'instagram', 'taylorswift', 'selenagomez', 'kimkardashian'],
-        source: "http://iconosquare.com/",
-        selector: [
-            'a[class="followers user-action-btn"] span[class=chiffre]',
-            'a[class="followings user-action-btn"] span[class=chiffre]'
-        ]
-    };
-
-    // LOOP: grab data from source
-    async.forever(function (next) {
-        log('I am in async.forever(). Waiting ' + delayInMs(settings.desiredTime) + 'ms.');
-        setTimeout(function() {
-            log('Timeout over. Calling getRemoteData().');
-            getRemoteData(settings.source, settings.usernames[0], settings.selector, next);
-        }, delayInMs(settings.desiredTime)); // 5000 delayInMs(settings.desiredTime)
-    }, function (err) {
-        // error handling
-        error(err.message);
-    });
-}
-
-
-// get data from source, save data to DB
-function getRemoteData(source, username, selector, callback) {
-    var userUrl = source + username;
-    // send request
-    request(userUrl, {
-            timeout: 10000
-        },
-        function (err, res, body) {
-            if (err) {
-                error(err.message);
-            } else {
-                if (res.statusCode === 200) {
-                    var $ = cheerio.load(body);
-                    var timestamp = new Date();
-                    var newData = {
-                        timestamp: timestamp.getTime().toString(),
-                        followers: $(selector[0]).html().toString(),
-                        followings: $(selector[1]).html().toString()
-                    };
-
-                    // print data to console
-                    log(userUrl);
-                    log(JSON.stringify(newData, null, '\t'));
-
-                    // save data
-                    saveData(newData, username);
-                } else {
-                    error(res.statusCode);
-                }
-            }
-            // continue the loop
-            callback();
-        }
-    );
-}
-
-/*
- expects an array desiredTime = [hh, mm]
- returns the delay neccessary in ms till the desiredTime
- Formula: 
- x: current time in ms
- y: desired time in ms
- x >= y: (24 - x) + y
- x <  y: y - x
- */
-function delayInMs(desiredTime) {
-    var currentTime = new Date();
-    var x = (currentTime.getHours() * 60 + currentTime.getMinutes()) * 60 * 1000;
-    var y = (desiredTime[0] * 60 + desiredTime[1]) * 60 * 1000;
-
-    if (x >= y) {
-        return (86400000 - x) + y;
-    } else {
-        return y - x;
-    }
-}
-function delayInMsDEBUG(desiredTime, currentTime) {
-    var x = (currentTime[0] * 60 + currentTime[1]) * 60 * 1000;
-    var y = (desiredTime[0] * 60 + desiredTime[1]) * 60 * 1000;
-
-    if (x >= y) {
-        return (86400000 - x) + y;
-    } else {
-        return y - x;
-    }
 }
 
 
