ba1ff2e9eabd59c6c3777059dcb08aed7be69a2e
Yuri Gagarin <2g@space.pew>
2016-02-12 11:20:08 +0100

    added testMenuObj to the front end, pasing to scope, accesing in the view works

--- app/js/controllers/navigationctrl.js
+++ app/js/controllers/navigationctrl.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = function ($scope, $http, dataShare) {
-    
+
     // broadcast selected navigation item
     $scope.sendDataFromNavigationCtrl = function (item) {
         dataShare.sendData(item);
     }
-    
+
     // requests usernames for navigation from the be
     $http.get('/nav').success(function (response) {
         $scope.usernames = response;
@@ -15,4 +15,24 @@ module.exports = function ($scope, $http, dataShare) {
         // error handling
         console.log('Error: ' + error_response.status);
     });
+    
+    var testMenuObj = {
+        "user1": {
+            "2016": ["January", "February", "March"]
+        },
+        "user2": {
+            "2016": ["January", "February", "March"],
+            "2015": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        },
+    };
+    $scope.shizzle = testMenuObj;
 }
+
+
+
+
+
+
+
+
+
