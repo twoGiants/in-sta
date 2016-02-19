4475689b3d60cc6a751f2d4fef0ddf6f0fae4e51
Yuri Gagarin <2g@space.pew>
2016-02-19 16:41:46 +0100

    debug: the view selected the wrong field in the response object by default, fixed

--- app/js/controllers/tablectrl.js
+++ app/js/controllers/tablectrl.js
@@ -4,7 +4,7 @@ module.exports = function ($scope, $filter, $http, dataShare, statTools) {
     // call on load
     $http.get('/statistics').success(function (response) {
         // setup --------------------------------------------------------
-        $scope.data = response[2]; // select which collection to display
+        $scope.data = response[0]; // select which collection to display
 //        $scope.quantity = 21;      // how many rows to display
         statTools.calcGrowth($scope.data.ig_user_statistics); // calc and set growt
         
